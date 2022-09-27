965b609dba33fa7ce11eef6a2fad150574410938
Jared Conner <jared.w.conner@gmail.com>
2022-09-27 09:02:30 -0500

    Completed exercise

---
+++ script.js
@@ -0,0 +1,51 @@
+// STEP ONE
+
+console.log(3);
+console.log("My favorite number is three!");
+
+// STEP TWO
+// Five main data types: Numbers, Strings, Boolean, Null, Undefined
+
+// STEP THREE
+//  ** : Exponents * : Multiplication / : Division % : Modulous + : Addition - : Subtraction
+
+// STEP FOUR
+console.log(5%4);
+console.log(5%2);
+
+// STEP FIVE
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// STEP SIX
+console.log("Hawk" + "eye");
+
+// STEP SEVEN
+console.log("Jared Conner".length);
+
+// STEP EIGHT
+console.log("Avengers: Endgame"[16]);
+
+// BONUS ONE
+console.log("\/");
+
+// BONUS TWO
+console.log(5 ** 4 - 9);
+
+// BONUS THREE
+console.log(("I".length + "Am".length + "Iron".length + "Man".length) % 9); 
+
+// BONUS FOUR
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// BONUS FIVE
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
